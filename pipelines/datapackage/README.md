f80de8c4ee76756db1811b3fffc4ad06cb653c02
Franko1307 <franciscogonzalez1307@gmail.com>
2018-05-29 23:53:56 -0700

    datapackage

---
+++ pipelines/datapackage/README.md
@@ -0,0 +1,7 @@
+# **Datapackage
+
+A template showing how to use Popper with datapackage.
+In this example, Popper makes sure all the requirements are fulfilled in order to use datapackage. **
+
+
+
---
+++ pipelines/datapackage/post-process.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# [wf] execute post-process.sh stage
+
+#Here, you should deal with your generated data and transform it to a csv file.
+
+#Our file is already a csv file so we don't need this stage
+# but I'll keep it in order to show you the recommended steps when working with popper.
