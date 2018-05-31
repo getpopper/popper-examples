2cdc26a5ff2306343474833631c62a2f1d2bb7f5
Rafael Castillo <rafael@trfs.me>
2018-05-31 11:37:27 -0700

    Add envvar variable

---
+++ pipelines/envvar-param/README.md
@@ -0,0 +1,6 @@
+# Parametrized pipelines
+
+This simple pipeline shows how one might parametrize a pipeline using
+environment variables. The pipeline will fail if the N_REPETITIONS
+envvar isn't set.
+
---
+++ pipelines/envvar-param/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# [wf] execute run.sh stage
+
+if [ -z "$N_REPETITIONS" ]; then
+  echo "N_REPETITIONS environment variable needs to be set"
+  exit 1
+fi
+
+for i in $(seq 1 $N_REPETITIONS)
+do
+  echo "Done with $i"
+done
+
