ad6eb2c637aea691e185a930f934aca06e2befd3
Rafael Castillo <rafael@trfs.me>
2018-05-31 13:03:14 -0700

    Comment run script

--- pipelines/envvar-param/run.sh
+++ pipelines/envvar-param/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # [wf] execute run.sh stage
 
+# Check that N_REPETITIONS is set
 if [ -z "$N_REPETITIONS" ]; then
+  # Output error message to stderr
   echo "N_REPETITIONS environment variable needs to be set" >&2
   exit 1
 fi
