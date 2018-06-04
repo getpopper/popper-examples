5b7c8ebef41cd3bb11fbc3499742d7c1988a2ca0
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-03 23:49:22 -0700

    data-world

---
+++ pipelines/data-world/update-data-world-repo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# [wf] execute update-dataworld-repo.sh stage
+
+# [wf] Installing dependencies
+if python -c "import datadotworld" &> /dev/null; then
+    echo 'requirements already installed.'
+else
+    echo 'Installing dependencies'
+    pip install datadotworld[pandas]
+fi
+
+# [wf] running scripts
+python scripts/update-dataworld-repo.py
