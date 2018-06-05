0a5289902e9d55e678e4f9094dd571a6abd31e3f
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-04 19:58:11 -0700

    updating travis env variables

--- pipelines/data-world/scripts/update-dataworld-repo.py
+++ pipelines/data-world/scripts/update-dataworld-repo.py
@@ -6,7 +6,7 @@ import os
     Script for uploading your local changes to data.world
 """
 
-with dw.open_remote_file(os.environ['DATA_WORLD_ACCOUNT'], os.environ['DATA_WORLD_CSV']) as w:
+with dw.open_remote_file('https://data.world/fran1307/2018-datadotworldbballstats', 'DataDotWorldBBallStats.csv') as w:
     input_file = open("data/DataDotWorldBBallStats.csv", "r")
     reader = csv.reader(input_file)
     writer = csv.writer(w, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL)
