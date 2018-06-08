7bf8e39d2ca9670e5cf39e0a44d085ee87291ef3
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-08 15:28:40 -0700

    pep8 issues

--- pipelines/validator/scripts/validate.py
+++ pipelines/validator/scripts/validate.py
@@ -7,7 +7,10 @@ lb, ub = pickle.load(f)
 f.close()
 
 # Checks the 95% CI.
-if lb < 0 <= ub and ("VALIDATOR_STATUS" not in os.environ or os.environ['VALIDATOR_STATUS'] != "OFF"):
+if lb < 0 <= ub \
+        and (
+        "VALIDATOR_STATUS" not in os.environ
+        or os.environ['VALIDATOR_STATUS'] != "OFF"):
     print("[true] populations A and B do have similar mean with 95% CI")
 else:
     print("[false] populations A and B do not have similar mean with 95% CI")
