fa174e01c32b5ebcb66846d8c6384c4cc2cb866e
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-08 17:56:30 -0700

    pip3 -> python3-pip

--- pipelines/datapackage/update-datapackage.sh
+++ pipelines/datapackage/update-datapackage.sh
@@ -2,12 +2,12 @@
 # [wf] execute update-datapackage.sh stage
 
 # [wf] Installing dependencies
-if python3.6 -c "import datapackage" &> /dev/null; then
+if python3 -c "import datapackage" &> /dev/null; then
     echo 'requirements already installed.'
 else
     echo 'Installing dependencies'
-    pip3.6 install datapackage
+    pip3 install datapackage
 fi
 
 # [wf] running scripts
-python3.6 scripts/update-datapackage.py
+python3 scripts/update-datapackage.py
