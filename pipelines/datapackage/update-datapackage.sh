97d698b5c2c4468173b040d83093f4d6b15ccecb
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-10 16:01:08 -0700

    python3.5 -> python3.4

--- pipelines/datapackage/update-datapackage.sh
+++ pipelines/datapackage/update-datapackage.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 # [wf] execute update-datapackage.sh stage
 
+# Makes sure a virtual env is active.
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "ERROR: A virtual environment must be active in order to execute this stage."
+    exit 1
+fi
+
 # [wf] Installing dependencies
-if python3.5 -c "import datapackage" &> /dev/null; then
+if python3.4 -c "import datapackage" &> /dev/null; then
     echo 'requirements already installed.'
 else
     echo 'Installing dependencies'
@@ -10,4 +16,4 @@ else
 fi
 
 # [wf] running scripts
-python3.5 scripts/update-datapackage.py
+python3.4 scripts/update-datapackage.py
