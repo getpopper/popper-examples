3fdd87e0c56bb190e1ca3928ebaace6fe9eed7bd
Rafael Castillo <rafael@trfs.me>
2018-06-12 14:07:11 -0700

    Adds charts

--- pipelines/pgbench/analyze.sh
+++ pipelines/pgbench/analyze.sh
@@ -6,5 +6,3 @@ mkdir -p figures
 
 docker run --rm -p 8888:8888 -v "$PWD":/experiment --workdir=/experiment --user=root --entrypoint=jupyter jupyter/scipy-notebook nbconvert --execute analyze.ipynb --ExecutePreprocessor.timeout=-1 --inplace
 
-
-
