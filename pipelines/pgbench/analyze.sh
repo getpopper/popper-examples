b5e3035a2179723a45f812e66208a86303a28143
Rafael Castillo <rafael@trfs.me>
2018-06-12 14:32:48 -0700

    Run notebook as root

--- pipelines/pgbench/analyze.sh
+++ pipelines/pgbench/analyze.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p figures
 
-docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan --entrypoint=jupyter jupyter/scipy-notebook nbconvert --execute analyze.ipynb --ExecutePreprocessor.timeout=-1 --inplace
+docker run --rm -p 8888:8888 -v "$PWD":/experiment --workdir=/experiment --user=root --entrypoint=jupyter jupyter/scipy-notebook nbconvert --execute analyze.ipynb --ExecutePreprocessor.timeout=-1 --inplace
 
 
 
