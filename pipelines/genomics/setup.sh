c3048af284b4138fc65f6d34685dc80e136b3ae3
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-26 06:00:18 -0700

    genomics: makes sure container is off

--- pipelines/genomics/setup.sh
+++ pipelines/genomics/setup.sh
@@ -3,9 +3,13 @@
 # Moves to the docker directory.
 cd docker
 
+# Makes sure
+docker stop genomic_container
+docker rm genomic_container
+
 # build genomics container.
 docker build -t genomics .
 
-docker run -t -d --name=genomic_container genomics
+docker run -it --name=genomic_container genomics
 
 docker exec genomic_container ./docker-setup.sh
