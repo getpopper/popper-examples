a82fd4132526c0d341f92148d0e84977c4a966c6
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-29 21:46:17 -0700

    puts env-checker into run

--- pipelines/sea-surface-mapping-r/run.sh
+++ pipelines/sea-surface-mapping-r/run.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Checks if a virtual environment is active.
+if [ "$VIRTUAL_ENV" != "" ]; then
+    source "$VIRTUAL_ENV"/bin/activate
+    echo "It looks like you have an active virtual environment. I'll deactivate it in order to execute this pipeline."
+    deactivate
+fi
+
+# Create virtual environment using python 2.7
+virtualenv --python=python2.7 virtual-environment-python-27/
+
+source virtual-environment-python-27/bin/activate
+
+pip install scipy
+
 # [wf] Check if PLOTY_USERNAME environment variable is set
 if [ -z "$PLOTY_USERNAME" ]; then
   echo "ERROR: PLOTY_USERNAME environment variable needs to be set"
