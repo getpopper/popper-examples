ac86cb7310e6a33cdee3391a3effe3371281e362
Franko1307 <franciscogonzalez1307@gmail.com>
2018-06-30 08:00:49 -0700

    change virtual env setup

--- pipelines/sea-surface-mapping-r/setup.sh
+++ pipelines/sea-surface-mapping-r/setup.sh
@@ -20,23 +20,23 @@ result = tryCatch({
     packrat::init()
 
     # Checks if a virtual environment is active.
-    { if [ "$VIRTUAL_ENV" != "" ]; then
-        source "$VIRTUAL_ENV"/bin/activate
-        echo "It looks like you have an active virtual environment. I'll deactivate it in order to execute this pipeline."
-        deactivate
-    fi }
-
-    # Create virtual environment using python 2.7
-    virtualenv --python=python2.7 virtual-environment-python-27/
-
-    source virtual-environment-python-27/bin/activate
-
-    pip install scipy
-    pip install -U numpy
-
 
 })
 
+
+if [ "$VIRTUAL_ENV" != "" ]; then
+    source "$VIRTUAL_ENV"/bin/activate
+    echo "It looks like you have an active virtual environment. I'll deactivate it in order to execute this pipeline."
+    deactivate
+fi
+
+# Create virtual environment using python 2.7
+virtualenv --python=python2.7 virtual-environment-python-27/
+
+source virtual-environment-python-27/bin/activate
+
+pip install scipy
+pip install -U numpy
 
 
 
