e6354af1f592c9b2745d675b4a0f6b2805201e4e
Franko1307 <franciscogonzalez1307@gmail.com>
2018-07-05 15:02:20 -0700

    fix popper:whitelist[sea-surface-mapping-r] test

--- pipelines/sea-surface-mapping-r/docker/docker-run.sh
+++ pipelines/sea-surface-mapping-r/docker/docker-run.sh
@@ -5,6 +5,8 @@ python --version
 pip install -U scipy
 pip install -U numpy
 
+Rscript scripts/install_libraries_after_packrat.R
+
 # All required libraries required for your project.
 Rscript scripts/install_libraries_after_packrat.R
 
