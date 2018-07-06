430a4c99946c12980cd1374e4e9ffdcde9d845fd
Franko1307 <franciscogonzalez1307@gmail.com>
2018-07-06 11:46:59 -0700

    fix popper:whitelist[sea-surface-mapping-r]

--- pipelines/sea-surface-mapping-r/docker/scripts/packrat_requirements.R
+++ pipelines/sea-surface-mapping-r/docker/scripts/packrat_requirements.R
@@ -11,3 +11,4 @@ library(devtools)
 install_github("r-spatial/sf")
 
 install.packages("maptools", repos = "http://cran.us.r-project.org")
+install.packages("rgeos"   , repos = "http://cran.us.r-project.org")
