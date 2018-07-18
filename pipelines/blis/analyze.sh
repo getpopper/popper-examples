cb6547fc90981e057f6943fa45568830feb4e703
Franko1307 <franciscogonzalez1307@gmail.com>
2018-07-17 18:59:57 -0700

    Add stage validate

--- pipelines/blis/analyze.sh
+++ pipelines/blis/analyze.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e -x
 docker run --rm -p 8888:8888 -v "$PWD":/experiment --workdir=/experiment/results --user=root --entrypoint=jupyter smizy/octave:4.2.0-alpine nbconvert --execute visualize.ipynb --ExecutePreprocessor.timeout=-1 --inplace
-
---
+++ pipelines/blis/validate.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+set -e -x
+
+if [ -f results/gemv_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/ger_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/hemv_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her2_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trmv_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trsv_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/gemm_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/hemm_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/herk_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her2k_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trmm_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trsm_openblas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/gemv_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/ger_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/hemv_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her2_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trmv_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trsv_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/gemm_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/hemm_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/herk_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her2k_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trmm_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trsm_atlas.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/gemv_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/ger_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/hemv_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her2_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trmv_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trsv_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/gemm_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/hemm_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/herk_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/her2k_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trmm_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
+if [ -f results/trsm_blis.m ]; then
+  echo "[true] The file exists"
+else
+  echo "[false] The file doesn't exists"
+fi
