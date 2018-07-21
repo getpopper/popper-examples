35c71b424920e23a957e37176c8d0de2843ff2c8
Franko1307 <franciscogonzalez1307@gmail.com>
2018-07-20 17:28:05 -0700

    add analyze stage

--- pipelines/blis/analyze.sh
+++ pipelines/blis/analyze.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e -x
-docker run --rm -p 8888:8888 -v "$PWD":/experiment --workdir=/experiment/results --user=root --entrypoint=jupyter smizy/octave:4.2.0-alpine nbconvert --execute visualize.ipynb --ExecutePreprocessor.timeout=-1 --inplace
+docker run --rm -p 8888:8888 -v `pwd`:/experiment --workdir=/experiment/results --user=root --entrypoint=jupyter octave-alpine:3.5 nbconvert --execute visualize.ipynb --ExecutePreprocessor.timeout=-1 --inplace
