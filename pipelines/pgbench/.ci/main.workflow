f67457c99e2b313bae1bc657a6dfac01edc6c7d3
arshul <arshulmohd123@gmail.com>
2019-04-07 20:59:05 +0530

    added lint, script validated,disabled killing containers

--- pipelines/pgbench/.ci/main.workflow
+++ pipelines/pgbench/.ci/main.workflow
@@ -1,11 +1,16 @@
 workflow "Run Benchmarks" {
   resolves = "analyse"
 }
+action "lint" {
+  uses = "actions/bin/shellcheck@master"
+  args = "-x ./pipelines/pgbench/benchmark.sh ./pipelines/pgbench/analyze.sh"
+}
 action "benchmark" {
+  needs= "lint"
   uses = "actions/docker/cli@master"
   runs = ["sh", "-c", "./pipelines/pgbench/benchmark.sh"]
   env = {
-    PG_VERSIONS = "9.6,9.3"
+    PG_IMAGES = "pg9.6,pg9.3"
   }
 }
 action "analyse" {
--- pipelines/pgbench/benchmark.sh
+++ pipelines/pgbench/benchmark.sh
@@ -1,28 +1,24 @@
 #!/usr/bin/env bash
+set -ex
 
-PG_IMAGES=${PG_IMAGES:-$PG_VERSIONS}
-echo $PG_IMAGES
-rm $GITHUB_WORKSPACE/pipelines/pgbench/results_/*
+echo "$PG_IMAGES"
+rm "$GITHUB_WORKSPACE/pipelines/pgbench/results_/*"
 
 
-mkdir -p $GITHUB_WORKSPACE/pipelines/pgbench/results_
+mkdir -p "$GITHUB_WORKSPACE/pipelines/pgbench/results_"
 
-for image in $(echo $PG_IMAGES | sed "s/,/ /g")
+for image in ${PG_IMAGES//,/ }
 do
-  docker run --name pg$image -e POSTGRES_PASSWORD=pgbench -d postgres:$image
-  docker exec pg$image pg_isready -t 20
+  docker run --name "$image" -e POSTGRES_PASSWORD=pgbench -d "postgres:${image: -3}"
+  docker exec "$image" pg_isready -t 20
 
   for query_mode in simple extended prepared
   do
     for n_threads in 1 2 4 8 16 32
     do
       echo "Running pgbench for image $image with $n_threads threads and $query_mode query mode."
-      echo pgbench | docker run -i --rm --link pg$image:postgres postgres:$image pgbench -i -h postgres -U postgres
-      echo pgbench | docker run -i --rm --link pg$image:postgres postgres:$image pgbench -h postgres -U postgres -M $query_mode -t $n_threads > $GITHUB_WORKSPACE/pipelines/pgbench/results_/$image-$n_threads-$query_mode.results
+      echo pgbench | docker run -i --rm --link "$image":postgres "postgres:${image: -3}" pgbench -i -h postgres -U postgres
+      echo pgbench | docker run -i --rm --link "$image":postgres "postgres:${image: -3}" pgbench -h postgres -U postgres -M "$query_mode" -t "$n_threads" > "$GITHUB_WORKSPACE/pipelines/pgbench/results_/$image-$n_threads-$query_mode.results"
     done
   done
-  echo "Killing container for postgres $image"
-  docker kill pg$image
-  echo "Removing container for postgres $image"
-  docker rm pg$image
 done
