db88e81701b6750345a555aa823ab699916a7cff
arshul <arshulmohd123@gmail.com>
2019-05-09 14:58:58 +0530

    connection issue fix

--- pipelines/pgbench/analyze.sh
+++ pipelines/pgbench/analyze.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -ex
 
-mkdir -p figures
+mkdir -p "$GITHUB_WORKSPACE/pipelines/pgbench/figures"
 
-docker run --rm -p 8888:8888 -v "$PWD":/experiment --workdir=/experiment --user=root --entrypoint=jupyter jupyter/scipy-notebook nbconvert --execute "$GITHUB_WORKSPACE"/pipelines/pgbench/analyze.ipynb --ExecutePreprocessor.timeout=-1 --inplace
+docker run --rm -p 8888:8888 -v "$GITHUB_WORKSPACE":/experiment --workdir=/experiment --user=root --entrypoint=jupyter jupyter/scipy-notebook nbconvert --execute pipelines/pgbench/analyze.ipynb --ExecutePreprocessor.timeout=-1 --inplace
 
--- pipelines/pgbench/benchmark.sh
+++ pipelines/pgbench/benchmark.sh
@@ -2,14 +2,15 @@
 set -ex
 
 echo "$PG_IMAGES"
-rm -rf "$GITHUB_WORKSPACE/pipelines/pgbench/results_/"
+rm -rf "$GITHUB_WORKSPACE/pipelines/pgbench/results/"
 
 
-mkdir -p "$GITHUB_WORKSPACE/pipelines/pgbench/results_"
+mkdir -p "$GITHUB_WORKSPACE/pipelines/pgbench/results"
 
 for image in $(echo "$PG_IMAGES" | sed "s/,/ /g")
 do
   docker run --name "$image" -e POSTGRES_PASSWORD=pgbench -d postgres:"$(echo "$image" | tail -c 4)"
+  sleep 5
   docker exec "$image" pg_isready -t 20
 
   for query_mode in simple extended prepared
@@ -18,7 +19,7 @@ do
     do
       echo "Running pgbench for image $image with $n_threads threads and $query_mode query mode."
       echo pgbench | docker run -i --rm --link "$image":postgres postgres:"$(echo "$image" | tail -c 4)" pgbench -i -h postgres -U postgres
-      echo pgbench | docker run -i --rm --link "$image":postgres postgres:"$(echo "$image" | tail -c 4)" pgbench -h postgres -U postgres -M "$query_mode" -t "$n_threads" > "$GITHUB_WORKSPACE"/pipelines/pgbench/results_/"$image"-"$n_threads"-"$query_mode".results
+      echo pgbench | docker run -i --rm --link "$image":postgres postgres:"$(echo "$image" | tail -c 4)" pgbench -h postgres -U postgres -M "$query_mode" -t "$n_threads" > "$GITHUB_WORKSPACE"/pipelines/pgbench/results/"$image"-"$n_threads"-"$query_mode".results
     done
   done
 done
