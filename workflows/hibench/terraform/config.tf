25ba5261865432b601770627ac05546252fe2f50
arshul <arshulmohd123@gmail.com>
2019-05-29 02:54:45 +0530

    docker image for master, config.tf  creates workers and master node

--- workflows/hibench/terraform/config.tf
+++ workflows/hibench/terraform/config.tf
@@ -1,12 +1,13 @@
 variable "PACKET_API_KEY" {}
+variable "WORKERS_COUNT" { default = 1}
 provider "packet" {
   auth_token = "${var.PACKET_API_KEY}"
 }
 locals {
   project_id = "ead345e7-4525-4d09-bf49-c8b2b6e0f9cf"
 }
-resource "packet_device" "hibench" {
-  hostname="hibench"
+resource "packet_device" "spark_master" {
+  hostname="spark.master"
   project_id       = "${local.project_id}"
   operating_system = "ubuntu_16_04"
   plan = "t1.small.x86"
@@ -14,10 +15,26 @@ resource "packet_device" "hibench" {
   facilities = ["ewr1"]
 
   provisioner "local-exec" {
-    command = "echo ${packet_device.hibench.access_public_ipv4} > ../ansible/hosts.ini"
+    command = "cat <<EOF > ../ansible/hosts.ini\n[master]\n${self.access_public_ipv4}\n[workers]\nEOF"
   }
 
 }
-output "ip" {
-  value = packet_device.hibench.access_public_ipv4
+resource "packet_device" "spark_worker" {
+  count = "${var.WORKERS_COUNT}"
+  hostname="spark.worker.${count.index}"
+  project_id       = "${local.project_id}"
+  operating_system = "ubuntu_16_04"
+  plan = "t1.small.x86"
+  billing_cycle    = "hourly"
+  facilities = ["ewr1"]
+
+  provisioner "local-exec" {
+    command = "echo '${self.access_public_ipv4}\n' >> ../ansible/hosts.ini"
+  }
+  depends_on = [
+    packet_device.spark_master,
+  ]
+}
+output "master_ip" {
+  value = packet_device.spark_master.access_public_ipv4
 }
