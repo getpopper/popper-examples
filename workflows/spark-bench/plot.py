d2876a2ff6b6bb353beace6e12895591a9eeb32a
arshul <arshulmohd123@gmail.com>
2019-06-16 01:08:16 +0530

    minor fixes to finalise PR

--- workflows/spark-bench/plot.py
+++ workflows/spark-bench/plot.py
@@ -4,6 +4,11 @@ import seaborn as sns
 sns.set(style="whitegrid")
 df = pd.read_csv('./workflows/spark-bench/results/outA.csv')
 df = df.append(pd.read_csv('./workflows/spark-bench/results/outB.csv'))
-ax = sns.barplot(x="name", y="total_runtime", hue="description", data=df).get_figure().savefig('./workflows/spark'
-                                                                                               '-bench/results'
-                                                                                               '/income_f_age.png')
+ax = sns.barplot(
+    x="name",
+    y="total_runtime",
+    hue="description",
+    data=df
+). \
+    get_figure(). \
+    savefig('./workflows/spark-bench/results/income_f_age.png')
