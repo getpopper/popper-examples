2d4240b353c3a0140bc50a22c097f57dc955d917
Mohd Arshul Mansoori <arshulmohd123@gmail.com>
2019-06-21 12:42:52 +0530

    Adds workflow for benchmarking of Spark (#26)
    Adds workflow for running a spark benchmark on Packet baremetal machines.
    
    popper:whitelist[workflows/spark-bench/main.workflow]
    
    fixes systemslab/popper#590

---
+++ workflows/spark-bench/terraform/config.tf
@@ -0,0 +1,43 @@
+variable "PACKET_API_KEY" {}
+variable "WORKERS_COUNT" { default = 1 }
+
+provider "packet" {
+  auth_token = "${var.PACKET_API_KEY}"
+}
+
+locals {
+  project_id = "ead345e7-4525-4d09-bf49-c8b2b6e0f9cf"
+}
+
+resource "packet_device" "spark_master" {
+  hostname="spark.master"
+  project_id       = "${local.project_id}"
+  operating_system = "ubuntu_16_04"
+  plan = "t1.small.x86"
+  billing_cycle    = "hourly"
+  facilities = ["ewr1"]
+  provisioner "local-exec" {
+    command = "cat <<EOF > ./workflows/spark-bench/ansible/hosts.ini\n[master]\n${self.access_public_ipv4} ansible_user=root ansible_become=True\n[workers]\nEOF"
+
+  }
+
+}
+resource "packet_device" "spark_worker" {
+  count = "${var.WORKERS_COUNT}"
+  hostname="spark.worker.${count.index}"
+  project_id       = "${local.project_id}"
+  operating_system = "ubuntu_16_04"
+  plan = "t1.small.x86"
+  billing_cycle    = "hourly"
+  facilities = ["ewr1"]
+  provisioner "local-exec" {
+    command = "sleep ${count.index+5}s && cat <<EOF >>./workflows/spark-bench/ansible/hosts.ini\n${self.access_public_ipv4} ansible_user=root ansible_become=True\n"
+  }
+  depends_on = ["packet_device.spark_master"]
+}
+output "master_ip" {
+  value = "${packet_device.spark_master.access_public_ipv4}"
+}
+output "worker_ips" {
+  value = "${packet_device.spark_worker.*.access_public_ipv4}"
+}
---
+++ workflows/spark-bench/validate.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import math
+import seaborn as sns
+
+df = pd.read_csv('./workflows/spark-bench/results/outA.csv')
+df = df.assign(difference=abs(df['pi_approximate'] - math.pi))
+
+is_10 = (df['slices'] == 10)
+is_100 = (df['slices'] == 100)
+is_1000 = (df['slices'] == 1000)
+
+mean_10 = df[is_10]["difference"].mean()
+mean_100 = df[is_100]["difference"].mean()
+mean_1000 = df[is_1000]["difference"].mean()
+
+perc_error_10 = (mean_10/math.pi) * 100
+perc_error_100 = (mean_100/math.pi) * 100
+perc_error_1000 = (mean_1000/math.pi) * 100
+
+print("Percent Error with 10 slices: {:.4f}%".format(perc_error_10))
+print("Percent Error with 100 slices: {:.4f}%".format(perc_error_100))
+print("Percent Error with 1000 slices: {:.4f}%".format(perc_error_1000))
+
+ax = sns.barplot(
+    x="name",
+    y="difference",
+    hue="slices",
+    data=df
+). \
+    get_figure(). \
+    savefig('./workflows/spark-bench/results/income_f_age.png')
+
+
+assert (perc_error_10 > perc_error_100) and (perc_error_100 > perc_error_1000)
