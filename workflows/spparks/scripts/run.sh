71ea4d45bf721036fc7535c53911bbf6622d632f
arshul <arshulmohd123@gmail.com>
2019-07-30 15:57:48 +0530

    build inside container popper:run[--wfile workflows/spparks/main.workflow]

--- workflows/spparks/scripts/run.sh
+++ workflows/spparks/scripts/run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-spk=$GITHUB_WORKSPACE/workflows/spparks/SPPARKS/src/spk_demo_serial
+pushd workflows/spparks/submodules/sppark-demo/demos/visualization
+$spk < viz.spkin
+python3 -m venv ./venv
+source ./venv/bin/activate
 
-$spk < workflows/spparks/submodules/sppark-demo/demos/basic/basic.spkin
+pip3 install --no-cache-dir "numpy>=1.12.0"
+dump 1.0 text potts.dump
+
+python pizza_dump2vtk.py potts.dump
