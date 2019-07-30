b4ab1bde08449371ab614b7cad9c6d1834275760
arshul <arshulmohd123@gmail.com>
2019-07-30 16:07:39 +0530

    fix python deps popper:run[--wfile workflows/spparks/main.workflow]

--- workflows/spparks/scripts/run.sh
+++ workflows/spparks/scripts/run.sh
@@ -5,7 +5,7 @@ $spk < viz.spkin
 python3 -m venv ./venv
 source ./venv/bin/activate
 
-pip3 install --no-cache-dir "numpy>=1.12.0"
+pip install --no-cache-dir "numpy>=1.12.0"
 dump 1.0 text potts.dump
 
 python pizza_dump2vtk.py potts.dump
