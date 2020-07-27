61194996954682a805a6fdb7d1c386bd61457599
Anders Poirel <andersjopo@gmail.com>
2020-07-27 14:37:11 -0700

    fixed data download

--- workflows/comp-research/python/src/get_data.sh
+++ workflows/comp-research/python/src/get_data.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd ../data/raw
+cd $1
 
-wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/test_set_features.csv"
-wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_labels.csv"
-wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_features.csv"
+wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/test_set_features.csv" --no-check-certificate
+wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_labels.csv" --no-check-certificate
+wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_features.csv" --no-check-certificate
 
 echo "Files downloaded: $(ls)"
