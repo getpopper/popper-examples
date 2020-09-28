04b56be4f6ddf1c409249cd7f235389a2e2c46f4
Anders Poirel <andersjopo@gmail.com>
2020-09-28 10:40:15 -0700

    new organization

--- workflows/comp-research/rstudio/src/get_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd $1
-
-wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/test_set_features.csv" --no-check-certificate
-wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_labels.csv" --no-check-certificate
-wget "https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_features.csv" --no-check-certificate
-
-echo "Files downloaded: $(ls)"
---
+++ workflows/comp-research/rstudio/src/test.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+
+print("Example script")
