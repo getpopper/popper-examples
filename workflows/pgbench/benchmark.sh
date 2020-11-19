7887b28ecd868c2ccbe6919591d390a400a53af6
Ivo Jimenez <ivo.jimenez@gmail.com>
2020-11-19 03:48:40 +0000

    Fixes pgbench example

--- workflows/pgbench/benchmark.sh
+++ workflows/pgbench/benchmark.sh
@@ -1,58 +1,37 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -ex
 
-if [ -z "$PG_IMAGES" ]; then
-  echo "Expecting PG_IMAGES variable"
-  exit 1
-fi
 if [ -z "$PG_SCALE_FACTOR" ]; then
   echo "Expecting PG_SCALE_FACTOR variable"
   exit 1
 fi
 
-outdir="$GITHUB_WORKSPACE/workflows/pgbench/results"
-
-rm -rf "$outdir"
+outdir="./results"
 mkdir -p "$outdir"
 
-# get the list of images to test
-images=$(echo "$PG_IMAGES" | sed 's/ //g' | sed 's/,/ /g')
+export POSTGRES_PASSWORD=pgbench
 
-for image in $images; do
-  # get container name
-  cid="pgbench-$(echo "$image" | sed 's/:/-/')"
+bash -c 'cd / && docker-entrypoint.sh postgres &'
 
-  # stop (if previous was running)
-  docker stop "$cid" || true
+# give docker some time to instantiate the container
+sleep 10
 
-  # create container
-  docker run --rm --name "$cid" -e POSTGRES_PASSWORD=pgbench -d "$image"
+# wait for server to boot
+pg_isready -t 20
 
-  # give docker some time to instantiate the container
-  sleep 10
+# initialize test database
+echo pgbench | pgbench -i -s "$PG_SCALE_FACTOR" -U postgres
 
-  # wait for server to boot
-  docker exec "$cid" pg_isready -t 20
+version=$(postgres -V | awk '{ print $3 }')
 
-  # initialize test database
-  echo pgbench | docker run -i --rm \
-    --link "$cid":postgres "$image" \
-      pgbench -i -s "$PG_SCALE_FACTOR" -h postgres -U postgres
+for mode in simple extended prepared; do
+  for threads in 1 2 4 8; do
 
-  for mode in simple extended prepared; do
-    for threads in 1 2 4 8; do
+    outfile="$outdir/$version-$threads-$mode.results"
 
-      outfile="$outdir/$image-$threads-$mode.results"
+    echo "Running postgres-$version with $threads threads and $mode query mode."
 
-      echo "Running $image with $threads threads and $mode query mode."
-
-      # run benchmark
-      echo pgbench | docker run -i --rm \
-        --link "$cid":postgres "$image" \
-          pgbench -h postgres -U postgres -M "$mode" -t "$threads" > "$outfile"
-    done
+    # run benchmark
+    echo pgbench | pgbench -U postgres -M "$mode" -t "$threads" > "$outfile"
   done
-
-  # stop the container
-  docker stop "$cid"
 done
